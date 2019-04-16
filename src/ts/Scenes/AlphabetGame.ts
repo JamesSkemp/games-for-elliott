06d0a39ecdb7ceb7a1daf1625dd688a5972444a3
James Skemp <strivinglife@gmail.com>
2019-04-16 15:18:18 -0500

    Update alphabet display to not be just black and white

--- src/ts/Scenes/AlphabetGame.ts
+++ src/ts/Scenes/AlphabetGame.ts
@@ -10,6 +10,8 @@ export default class AlphabetGame extends Phaser.Scene {
 	 */
 	private soundPlaying: boolean = false;
 	private alphabetSound;
+	private currentColor: number = 0;
+	private colors = ["red", "yellow", "green", "blue"];
 
 	public preload(): void {
 		// Preload as needed.
@@ -17,6 +19,7 @@ export default class AlphabetGame extends Phaser.Scene {
 
 	public create(): void {
 		Utilities.LogSceneMethodEntry("AlphabetGame", "create");
+		this.cameras.main.setBackgroundColor("#333");
 
 		const row1Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 		const row2Letters = ["J", "K", "L", "M", "N", "O", "P", "Q", "R"];
@@ -29,24 +32,32 @@ export default class AlphabetGame extends Phaser.Scene {
 		for (let i = 0; i < row1Letters.length; i++) {
 			this.add.text(leftPadding + letterSpacing * i, 100, row1Letters[i])
 				.setFontFamily("Carlito").setFontSize(letterSize).setOrigin(0.5)
+				.setColor(this.colors[this.currentColor])
 				.setInteractive().on("pointerdown", () => { this.playLetter(row1Letters[i]); }, this);
+			this.incrementCurrentColor();
 		}
 
 		for (let i = 0; i < row2Letters.length; i++) {
 			this.add.text(leftPadding + letterSpacing * i, 225, row2Letters[i])
 				.setFontFamily("Carlito").setFontSize(letterSize).setOrigin(0.5)
+				.setColor(this.colors[this.currentColor])
 				.setInteractive().on("pointerdown", () => { this.playLetter(row2Letters[i]); }, this);
-		}
+			this.incrementCurrentColor();
+			}
 
 		for (let i = 0; i < row3Letters.length; i++) {
 			this.add.text(leftPadding + letterSpacing * i + (letterSpacing / 2), 350, row3Letters[i])
 				.setFontFamily("Carlito").setFontSize(letterSize).setOrigin(0.5)
+				.setColor(this.colors[this.currentColor])
 				.setInteractive().on("pointerdown", () => { this.playLetter(row3Letters[i]); }, this);
+			this.incrementCurrentColor();
 		}
 
 		this.add.text(this.cameras.main.centerX, 500, "SONG")
 			.setFontFamily("Carlito").setFontSize(letterSize).setOrigin(0.5)
+			.setColor(this.colors[this.currentColor])
 			.setInteractive().on("pointerdown", this.playSong, this);
+		this.incrementCurrentColor();
 	}
 
 	/**
@@ -60,7 +71,7 @@ export default class AlphabetGame extends Phaser.Scene {
 				song.destroy();
 			});
 			song.play();
-			console.log(arguments);
+			//console.log(arguments);
 		}
 		// TODO
 	}
@@ -72,16 +83,25 @@ export default class AlphabetGame extends Phaser.Scene {
 		if (!this.soundPlaying) {
 			this.soundPlaying = true;
 			const song = this.sound.add("alphabet_song_1_dad");
-			console.log(song.duration);
-			console.log(song.totalDuration);
+			//console.log(song.duration);
+			//console.log(song.totalDuration);
 			song.on("complete", (sound) => {
-				console.log(sound);
-				console.log("complete");
+				//console.log(sound);
+				//console.log("complete");
 				this.soundPlaying = false;
 				song.destroy();
 			});
-			console.log("Play Song");
 			song.play();
+		}
+	}
+
+	/**
+	 * Increase the current color variable so that the next usage pulls the proper color.
+	 */
+	private incrementCurrentColor(): void {
+		this.currentColor++;
+		if (this.currentColor >= this.colors.length) {
+			this.currentColor = 0;
 		}
 	}
 }
